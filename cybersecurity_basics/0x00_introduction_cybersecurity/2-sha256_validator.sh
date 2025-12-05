b72c586d9aaba22c0794ea9ea4339711cefb52fb
kanga-prog <kangabricekouakou@gmail.com>
2025-12-05 09:42:19 +0100

    Constructor of shar256

--- cybersecurity_basics/0x00_introduction_cybersecurity/2-sha256_validator.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/2-sha256_validator.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-[ "$(sha256sum "$1" | cut -d ' ' -f1)" = "$2" ] && echo "# $1: OK" || echo "# $1: NOT OK"
+[ "$(sha256sum "$1" | cut -d' ' -f1)" = "$2" ] && echo "$1: OK" || echo "$1: FAIL"
 
