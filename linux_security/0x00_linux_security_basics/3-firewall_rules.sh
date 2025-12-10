cb1ebe01698737ffd3ebf46db51da24052f25447
kanga-prog <kangabricekouakou@gmail.com>
2025-12-11 00:39:06 +0100

    a bash script that list all the rules in the security table of the firewal

--- linux_security/0x00_linux_security_basics/3-firewall_rules.sh
+++ linux_security/0x00_linux_security_basics/3-firewall_rules.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-iptables -t security -L -n -v # List all rules in security table with verbose output
+sudo iptables -t security -L -n -v  # List all rules in security table with verbose output
