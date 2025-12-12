21a03d3f5a3d6567ce59c2c9b5c22a620bac1d7d
kanga-prog <kangabricekouakou@gmail.com>
2025-12-12 15:45:18 +0100

    a bash script that extract using whois

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois $1 | grep -E "Registrant|Admin|Tech" > $1.csv
+whois $1 | awk -F': ' '/^(Registrant|Admin|Tech)/ { k=$1; v=$2; gsub(/^ +| +$/,"",v); if(k ~ /Street/) v=v" "; if(k ~ /Ext$/) printf("%s:,%s\n",k,""); else printf("%s,%s\n",k,v) }' > $1.csv
