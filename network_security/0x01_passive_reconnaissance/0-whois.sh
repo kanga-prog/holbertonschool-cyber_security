39824b7912a8c327f8f6bbae3ee35e33303c2f32
kanga-prog <kangabricekouakou@gmail.com>
2025-12-12 19:25:30 +0100

    a bash script that extract using whois

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$1" | awk -F': ' '/^Registrant |^Admin |^Tech / {gsub(/^[ \t]+|[ \t]+$/, "", $2) && ($1 ~ /Street/) $2=$2" "; print $1 "," $2}' > "$1.csv"
+whois $1 | awk -F': ' '/^(Registrant|Admin|Tech)/ {if ($1 ~ /Street/) $2=$2" "; gsub(/^[ \t]+|[ \t]+$/, "", $2); gsub(/ /,"$", $1); gsub(/ /,"$", $2); print $1 "," $2}' > "$1.csv"
