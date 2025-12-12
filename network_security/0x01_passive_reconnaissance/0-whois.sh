75e3d274387b5f995f0431e165c2547cbf5f79e3
kanga-prog <kangabricekouakou@gmail.com>
2025-12-12 16:21:45 +0100

    a bash script that extract using whois

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,29 @@
 #!/bin/bash
-whois $1 | awk -F': ' '/^(Registrant|Admin|Tech)/ { k=$1; v=$2; gsub(/^ +| +$/,"",v); if(k ~ /Street/) v=v" "; if(k ~ /Ext$/) printf("%s:,%s\n",k,""); else printf("%s,%s\n",k,v) }' > $1.csv
+# 0-whois.sh
+# Usage: ./0-whois.sh domain.com
+
+whois $1 | awk '
+BEGIN { OFS="," }
+# Registrant fields
+/^Registrant Name:/ {print "Registrant Name",$2}
+/^Registrant Organization:/ {print "Registrant Organization",$2}
+/^Registrant Street:/ {print "Registrant Street",$2" "}
+# Admin fields
+/^Admin Name:/ {print "Admin Name",$2}
+/^Admin Organization:/ {print "Admin Organization",$2}
+/^Admin Street:/ {print "Admin Street",$2" "}
+# Tech fields
+/^Tech Name:/ {print "Tech Name",$2}
+/^Tech Organization:/ {print "Tech Organization",$2}
+/^Tech Street:/ {print "Tech Street",$2" "}
+# Common fields
+/^City:/ {print $1,$2}
+/^State\/Province:/ {print $1,$2}
+/^Postal Code:/ {print $1,$2}
+/^Country:/ {print $1,$2}
+/^Phone:/ {print $1,$2}
+/^Phone Ext:/ {print $1","}   # Always include colon
+/^Fax:/ {print $1","}
+/^Fax Ext:/ {print $1","}
+/^Email:/ {print $1,$2}
+' | sed '$!s/$/\n/' > "$1.csv"
