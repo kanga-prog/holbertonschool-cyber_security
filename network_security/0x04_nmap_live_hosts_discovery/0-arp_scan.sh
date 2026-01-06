37e32468421157c16732ec744afa80ef75785f49
kanga-prog <kangabricekouakou@gmail.com>
2026-01-06 01:06:49 +0100

    a bash script that scan a subnetwork to discover live host using ICMP Echo scan

--- network_security/0x04_nmap_live_hosts_discovery/0-arp_scan.sh
+++ network_security/0x04_nmap_live_hosts_discovery/0-arp_scan.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-
-# Check if a subnetwork is provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 <subnetwork>"
-    exit 1
-fi
-
-# Run ARP scan using nmap (host discovery only, no port scan)
-sudo nmap -sn -PR "$1"
+sudo nmap -sn -PR $1
