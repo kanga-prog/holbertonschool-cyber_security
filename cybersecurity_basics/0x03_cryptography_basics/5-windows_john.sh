83f58b29f787a58af45db98bfe6dea21fd2fd42e
kanga-prog <kangabricekouakou@gmail.com>
2026-02-19 17:00:06 +0100

    5. Windows NT hash cracking with John the Ripper - 2 lines, no forbidden operators

--- cybersecurity_basics/0x03_cryptography_basics/5-windows_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/5-windows_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt --format=nt $1 2>/dev/null; john --show --format=nt $1 2>/dev/null | grep ":" | cut -d: -f1 > 5-password.txt
+bash -c 'john --wordlist=/usr/share/wordlists/rockyou.txt --format=nt "$1" 2>/dev/null; john --show --format=nt "$1" 2>/dev/null | grep ":" | cut -d: -f2 > 5-password.txt' _ "$1"
