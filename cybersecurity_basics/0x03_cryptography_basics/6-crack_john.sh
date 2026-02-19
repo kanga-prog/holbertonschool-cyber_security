e8bf8780c8b20dc664e3943adab8e42498fc627f
kanga-prog <kangabricekouakou@gmail.com>
2026-02-19 19:28:53 +0100

    Task 6: SHA256 hash cracking with John the Ripper - 2 lines, raw-sha256 format

--- cybersecurity_basics/0x03_cryptography_basics/6-crack_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/6-crack_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt --format=raw-sha256 "$1" 2>/dev/null; john --show --format=raw-sha256 "$1" 2>/dev/null | grep ":" | cut -d: -f1 > 6-password.txt _ "$1"
+john --wordlist=/usr/share/wordlists/rockyou.txt --format=Raw-SHA256 $1
