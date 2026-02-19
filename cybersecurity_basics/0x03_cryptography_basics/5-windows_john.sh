d86e7d940c1130ec0e39fa5323db01a9a1576fcb
kanga-prog <kangabricekouakou@gmail.com>
2026-02-19 18:47:03 +0100

    Task 5: Windows NT hash cracking

--- cybersecurity_basics/0x03_cryptography_basics/5-windows_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/5-windows_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-bash -c 'john --wordlist=/usr/share/wordlists/rockyou.txt --format=nt "$1" 2>/dev/null; john --show --format=nt "$1" 2>/dev/null | grep ":" | cut -d: -f2 > 5-password.txt' _ "$1"
+john --wordlist=/usr/share/wordlists/rockyou.txt --format=NT $1
