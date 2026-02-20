d5125eb57d016637c6b0b1485d6d0973891fde8d
kanga-prog <kangabricekouakou@gmail.com>
2026-02-20 11:52:26 +0100

    Windows Authentication Cracking

--- cybersecurity_basics/0x03_cryptography_basics/5-windows_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/5-windows_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt --format=nt "$1" 2>/dev/null && john --show --format=nt "$1" 2>/dev/null | grep ":" | cut -d: -f2 > 5-password.txt
+john --wordlist=/usr/share/wordlists/rockyou.txt --format=nt $1 2>/dev/null; john --show --format=nt $1 2>/dev/null | grep ":" | cut -d: -f1 > 5-password.txt
