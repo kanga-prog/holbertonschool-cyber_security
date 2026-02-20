0bf374621a2cfbada6bfbdc50924753465969122
kanga-prog <kangabricekouakou@gmail.com>
2026-02-20 23:23:22 +0100

    task6

--- cybersecurity_basics/0x03_cryptography_basics/6-crack_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/6-crack_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --wordlist=rockyou --format=sha256 "$1"
+john --wordlist=/usr/share/wordlists/rockyou.txt --format=raw-sha256 "$1" 2>/dev/null && john --show --format=raw-sha256 "$1" 2>/dev/null | cut -d: -f2 > 6-password.txt
