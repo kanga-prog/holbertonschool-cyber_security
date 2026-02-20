67f5928376dc912bd6d193c94c7d67bebb1eda0d
kanga-prog <kangabricekouakou@gmail.com>
2026-02-20 18:45:36 +0100

    Task 5: windows john

--- cybersecurity_basics/0x03_cryptography_basics/5-windows_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/5-windows_john.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt --format=NT $1
+john --wordlist=/usr/share/wordlists/rockyou.txt --format=nt "$1" 2>/dev/null
+cut -d: -f2 ~/.john/john.pot > 5-password.txt
