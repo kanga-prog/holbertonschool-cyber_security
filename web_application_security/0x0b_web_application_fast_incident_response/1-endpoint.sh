d7d7bb4e83bfc6bfcf7c2fdff6d30481af4b04af
kanga-prog <kangabricekouakou@gmail.com>
2026-04-02 19:02:09 +0200

    task1

--- web_application_security/0x0b_web_application_fast_incident_response/1-endpoint.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/1-endpoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-awk -F'"' '{print $2}' $1 | awk '{print $2}' | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}'
+grep -Eo 'POST [^ ]|GET [^ ]+' logs.txt | sort | uniq -c | sort -nr | head -n 1 | awk '{print $3}'
