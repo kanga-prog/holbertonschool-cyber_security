40b6a96f576d48d14ee0accc7d49b12cc012646a
kanga-prog <ton-email-github@example.com>
2026-05-06 22:45:25 +0200

    task 8

--- scripting_cyber/0x00-ruby_scripting/8-post.rb
+++ scripting_cyber/0x00-ruby_scripting/8-post.rb
@@ -1,20 +1,21 @@
-#!/usr/bin/env ruby
-
 require 'net/http'
 require 'uri'
 require 'json'
 
 def post_request(url, body_params)
-  uri = URI(url)
-  request = Net::HTTP::Post.new(uri)
-  request['Content-Type'] = 'application/json'
-  request.body = body_params.to_json
+  uri = URI.parse(url)
 
-  response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
-    http.request(request)
+  if uri.scheme == 'https'
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true 
+  else
+    http = Net::HTTP.new(uri.host, uri.port)
   end
 
-  puts "Response status: #{response.code} #{response.message}"
-  puts 'Response body:'
-  puts JSON.pretty_generate(JSON.parse(response.body))
+  request = Net::HTTP::Post.new(uri.path, { 'Content-Type' => 'application/json' })
+  request.body = body_params.to_json
+
+  response = http.request(request)
+  puts "Response status: #{response.code} #{response.message}" 
+  puts "Response body:\n#{response.body}"
 end
