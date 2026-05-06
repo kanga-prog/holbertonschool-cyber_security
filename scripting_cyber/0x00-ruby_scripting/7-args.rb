2e2ac4f8a1f9ba1c327996eed8af866a5f8c4f41
kanga-prog <ton-email-github@example.com>
2026-05-06 22:34:13 +0200

    task 7

--- scripting_cyber/0x00-ruby_scripting/7-args.rb
+++ scripting_cyber/0x00-ruby_scripting/7-args.rb
@@ -1,11 +1,10 @@
-#!/usr/bin/env ruby
-
 def print_arguments
   if ARGV.empty?
-    puts 'No arguments provided.'
+      puts "No arguments provided."
   else
-    ARGV.each_with_index do |arg, index|
-      puts "#{index + 1}. #{arg}"
-    end
+      puts "Arguments:"
+      ARGV.each_with_index do |arg, index|
+          puts "#{index +1}. #{arg}"
+      end
   end
 end
