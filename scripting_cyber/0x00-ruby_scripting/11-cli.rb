b34125b932d63f5a752549a91a618d7aae77a3d1
kanga-prog <ton-email-github@example.com>
2026-05-06 23:31:16 +0200

    task 11

--- scripting_cyber/0x00-ruby_scripting/11-cli.rb
+++ scripting_cyber/0x00-ruby_scripting/11-cli.rb
@@ -1,58 +1,67 @@
-#!/usr/bin/env ruby
-
 require 'optparse'
 
 TASKS_FILE = 'tasks.txt'
 
+File.write(TASKS_FILE, '', mode: 'a') unless File.exist?(TASKS_FILE)
+
 def add_task(task)
   File.open(TASKS_FILE, 'a') do |file|
-    file.puts task
+    file.puts(task)
   end
-
   puts "Task '#{task}' added."
 end
 
 def list_tasks
-  return unless File.exist?(TASKS_FILE)
-
-  File.readlines(TASKS_FILE).each_with_index do |task, index|
-    puts "#{index + 1}. #{task.chomp}"
+  tasks = File.readlines(TASKS_FILE).map(&:strip)
+  if tasks.empty?
+    puts 'No tasks available.'
+  else
+    puts 'Tasks:' # Encabezado requerido
+    tasks.each_with_index do |task, index|
+      puts "#{index + 1}. #{task}"
+    end
   end
 end
 
 def remove_task(index)
-  return unless File.exist?(TASKS_FILE)
-
-  tasks = File.readlines(TASKS_FILE)
-  removed_task = tasks.delete_at(index.to_i - 1)
-
-  File.open(TASKS_FILE, 'w') do |file|
-    tasks.each do |task|
-      file.puts task
-    end
+  tasks = File.readlines(TASKS_FILE).map(&:strip)
+  if index < 1 || index > tasks.size
+    puts 'Invalid index.'
+    return
   end
-
-  puts "Task '#{removed_task.chomp}' removed." if removed_task
+  removed_task = tasks.delete_at(index - 1)
+  File.write(TASKS_FILE, tasks.join("\n"))
+  puts "Task '#{removed_task}' removed."
 end
 
-options = OptionParser.new do |opts|
+options = {}
+OptionParser.new do |opts|
   opts.banner = 'Usage: cli.rb [options]'
 
   opts.on('-a', '--add TASK', 'Add a new task') do |task|
-    add_task(task)
+    options[:add] = task
   end
 
   opts.on('-l', '--list', 'List all tasks') do
-    list_tasks
+    options[:list] = true
   end
 
-  opts.on('-r', '--remove INDEX', 'Remove a task by index') do |index|
-    remove_task(index)
+  opts.on('-r', '--remove INDEX', Integer, 'Remove a task by index') do |index|
+    options[:remove] = index
   end
 
   opts.on('-h', '--help', 'Show help') do
     puts opts
+    exit
   end
-end
+end.parse!
 
-options.parse!
+if options[:add]
+  add_task(options[:add])
+elsif options[:list]
+  list_tasks
+elsif options[:remove]
+  remove_task(options[:remove])
+else
+  puts 'Use -h or --help for available options.'
+end
